ddf1690c2b22a96d6455dec313f3e051822d6a07
aducret <argentino.ducret@wolox.com.ar>
2017-05-22 14:36:48 -0300

    Removed ID

--- src/Main.elm
+++ src/Main.elm
@@ -1,46 +1,73 @@
 import Html exposing (..)
 import Html.Events exposing (..)
-import Random
 import Debug exposing (..)
+import Random
 
-main =
-  Html.beginnerProgram
-    { model = init
-    , view = view
-    , update = update
-    }
+main = Html.beginnerProgram
+   {
+      model   = initialModel,
+      view    = view,
+      update  = update
+   }
 
 -- MODEL
 
-type alias Point = (Int, Int)
+type alias Row = Int
+
+type alias Col = Int
+
+type alias Size = Int
+
+type alias Distance = Int
+
+type alias Point = (Row, Col)
+
+type alias Board = List(List(Tile))
 
 type alias Model =
-  { board : List(List(Tile))
-  , lastAction : Point
-  , winner : Winner
-  , turn : Turn
-  }
+   {
+      board           : Board,
+      selected        : Maybe Point,
+      match           : Match,
+      turn            : Turn
+   }
 
-type Tile = Empty (Int, Int) | RedBlob (Int, Int) Bool | BlueBlob (Int, Int) Bool | PossibleMovement (Int, Int)
+type Tile = Empty
+          | PossibleMovement
+          | Selected Blob
+          | Unselected Blob
 
-type Winner = BlueWin | RedWin | MatchInProgress -- TODO Empate
+type Blob = Blue
+          | Red
 
-type Turn = BlueTurn | RedTurn
+type Match = BlueWin
+           | RedWin
+           | Tie
+           | InProgress
 
-size : Int
+type Turn = BlueTurn
+          | RedTurn
+
+size : Size
 size = 8
 
-init : Model
-init = Model initializeBoard (0, 0) MatchInProgress BlueTurn
+initialBoard : Board
+initialBoard =
+   let
+      rows = (List.range 0 (size-1))
+      cols = (List.range 0 (size-1))
+      getBlob row col =
+         if (row, col) == (0, 0) || (row, col) == (0, size-1) then
+            Unselected Blue
+         else if (row, col) == (size-1, 0) || (row, col) == (size-1, size-1) then
+            Unselected Red
+         else
+            Empty
+   in
+      List.map (\row -> List.map (getBlob row) cols) rows
 
-initializeBoard : List(List(Tile))
-initializeBoard = List.map (\row -> (List.map (\col ->
-    if (row, col) == (0, 0) || (row, col) == (0, size-1) then
-      BlueBlob (row, col) False
-    else if (row, col) == (size-1, 0) || (row, col) == (size-1, size-1) then
-      RedBlob (row, col) False
-    else
-      Empty (row, col)) (List.range 0 (size-1)))) (List.range 0 (size-1))
+initialModel : Model
+initialModel = Model initialBoard Maybe.Nothing InProgress BlueTurn
 
 -- UPDATE
 
@@ -48,254 +75,144 @@ type Msg = Click Point
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Click (x, y) ->
-      case getTile x y model.board of
-        Just tile ->
-          case tile of
-            BlueBlob (x, y) False -> handleBlobSelection x y model
-            RedBlob (x, y) False -> handleBlobSelection x y model
-            Empty (x, y) -> { model | board = model.board |> clearPossibleMovements }
-            PossibleMovement (x, y) -> handleMovement x y model
-            BlueBlob (x, y) True -> { model | board = model.board |> clearPossibleMovements }
-            RedBlob (x, y) True -> { model | board = model.board |> clearPossibleMovements }
+   case Debug.log "msg" msg of
+      Click point ->
+         case getTile point model.board of
+            Just tile ->
+               case tile of
+                  Empty              -> model
+                  PossibleMovement   -> model
+                  Selected blob      -> model
+                  Unselected blob    -> model |> handleBlobSelection point
+            Nothing -> Debug.log "This should never happend!!!" model
 
-        Nothing -> model -- This should never happend
+getTile : Point -> Board -> Maybe Tile
+getTile point board =
+   let
+      row = Tuple.first point
+      col = Tuple.second point
+   in
+      case List.head (List.drop row board) of
+         Just cols   -> List.head (List.drop col cols)
+         Nothing     -> Nothing
 
-handleMovement : Int -> Int -> Model -> Model
-handleMovement x y model =
-  let newModel = moveBlob x y model
-  in
-    if checkFinalState newModel.board then
-      { newModel | winner = calculateWinner model.board }
-    else
-      newModel
-
-moveBlob : Int -> Int -> Model -> Model
-moveBlob x y model =
+handleBlobSelection : Point -> Model -> Model
+handleBlobSelection point model =
   let
-    distance = calculateDistance (Tuple.first model.lastAction) (Tuple.second model.lastAction) x y
+    tile = getTile point model.board
   in
-    if distance == 1 then
-      { model | board = model.board
-                           |> addBlobAt x y model.turn
-                           |> clearPossibleMovements,
-                turn = changeTurn model.turn }
-    else if distance == 2 then
-      { model | board = model.board
-                            |> removeBlobAt (Tuple.first model.lastAction) (Tuple.second model.lastAction) model.turn
-                            |> addBlobAt x y model.turn
-                            |> clearPossibleMovements,
-                turn = changeTurn model.turn }
-    else
-      { model | board = clearPossibleMovements model.board }
+    case (tile, model.turn) of
+      (Just (Unselected Red), RedTurn)   -> updateModelOnSelection point model
+      (Just (Unselected Blue), BlueTurn) -> updateModelOnSelection point model
+      (_, _)             -> model
 
--- TODO: Manchar
-addBlobAt : Int -> Int -> Turn -> List(List(Tile)) -> List(List(Tile))
-addBlobAt x y turn board =
-  List.map (\ tiles ->
-       List.map (\ tile ->
-            case tile of
-              Empty (x2, y2) ->
-                if x == x2 && y == y2 then
-                  createCurrentBlob turn x y
-                else
-                  tile
-              _ -> tile
-         ) tiles
-    ) board
+updateModelOnSelection : Point -> Model -> Model
+updateModelOnSelection point model =
+  let
+    updateBoard board =
+      board
+        |> updateBlob point selectTile
+  in
+    case model.selected of
+      Nothing    ->
+        {  model |
+           board = updateBoard model.board,
+           selected = Just point
+        }
+      Just blobPoint ->
+        {  model |
+           board = (updateBoard model.board)
+                          |> updateBlob blobPoint unselectTile,
+           selected = Just point
+        }
 
-removeBlobAt : Int -> Int -> Turn -> List(List(Tile)) -> List(List(Tile))
-removeBlobAt x y turn board =
-  List.map (\ tiles ->
-       List.map (\ tile ->
-            case tile of
-              RedBlob (x2, y2) _ ->
-                if x == x2 && y == y2 && turn == BlueTurn then
-                  Empty (x, y)
-                else
-                  tile
-              BlueBlob (x2, y2) _ ->
-                if x == x2 && y == y2 && turn == BlueTurn then
-                  Empty (x, y)
-                else
-                  tile
-              _ -> tile
-         ) tiles
-    ) board
+-- updateMovements : Maybe Blob -> (Tile -> Tile) -> Board -> Board
+-- updateMovements optionalBlolb transform board =
+--    case optionalBlob of
+--       Just blob ->
+--          let
+--             point = extractPointFromBlob blob
+--          in
+--             List.map (\ tiles ->
+--                List.map (\ tile ->
+--                   let
+--                      tilePoint = extractPointFromTile tile
+--                      distance = calculateDistance point tilePoint
+--                   in
+--                      if isSamePosition point tilePoint then
+--                         transform tile
+--                      else
+--                         tile
+--                ) tiles
+--             ) board
+--       Nothing -> board
 
-changeTurn : Turn -> Turn
-changeTurn turn =
-  case turn of
-    BlueTurn -> RedTurn
-    RedTurn -> BlueTurn
+-- Actualiza la blob dada en el tablero, aplicando la funciona dada.
+updateBlob : Point -> (Tile -> Tile) -> Board -> Board
+updateBlob point transform board =
+   let
+      update row col tile =
+        if point == (row, col) then
+           transform tile
+        else
+           tile
+   in
+      List.indexedMap (\row tiles -> List.indexedMap (update row) tiles) board
 
-createCurrentBlob : Turn -> Int -> Int -> Tile
-createCurrentBlob turn x y =
-  case turn of
-    BlueTurn -> BlueBlob (x, y) False
-    RedTurn -> RedBlob (x, y) False
+selectTile : Tile -> Tile
+selectTile tile =
+   case tile of
+      Unselected blob -> Selected blob
+      _ -> tile
 
-calculateDistance : Int -> Int -> Int -> Int -> Int
-calculateDistance x1 y1 x2 y2 =
-  max (abs (x1 - x2)) (abs (y1 - y2))
+unselectTile : Tile -> Tile
+unselectTile tile =
+   case tile of
+      Selected blob -> Unselected blob
+      _ -> tile
 
-handleBlobSelection : Int -> Int -> Model -> Model
-handleBlobSelection x y model =
-  if validSelection x y model.turn model.board then
-    { model | board = model.board |> clearPossibleMovements
-                                  |> showMovements x y,
-              lastAction = (x, y) }
-  else
-    { model | board = clearPossibleMovements model.board }
-
-checkFinalState : List(List(Tile)) -> Bool
-checkFinalState board =
-  (List.foldl (\tiles acum ->
-      acum + (List.foldl (\ tile acum ->
-          case tile of
-            Empty _ -> acum + 1
-            _ -> acum
-        ) 0 tiles)
-    ) 0 board) == 0
-
-calculateWinner : List(List(Tile)) -> Winner
-calculateWinner board =
-    let
-      red = List.foldl (\tiles acum ->
-          acum + (List.foldl (\ tile acum ->
-              case tile of
-                RedBlob _ _-> acum + 1
-                _ -> acum
-            ) 0 tiles)
-        ) 0 board
-
-      blue = List.foldl (\tiles acum ->
-          acum + (List.foldl (\ tile acum ->
-              case tile of
-                BlueBlob _ _-> acum + 1
-                _ -> acum
-            ) 0 tiles)
-        ) 0 board
-    in
-      if red > blue then
-        RedWin
-      else
-        BlueWin
-
-getTile : Int -> Int -> List(List(Tile)) -> Maybe Tile
-getTile x y board =
-  List.head (List.foldl (\tiles result ->
-      result ++ (List.foldl (\tile acum ->
-          case tile of
-            RedBlob (x2, y2) _ -> acum ++ (isTileAt x y x2 y2 tile)
-            BlueBlob (x2, y2) _ -> acum ++ (isTileAt x y x2 y2 tile)
-            PossibleMovement (x2, y2) -> acum ++ (isTileAt x y x2 y2 tile)
-            Empty (x2, y2) -> acum ++ (isTileAt x y x2 y2 tile)
-        ) [] tiles)
-    ) [] board)
-
-isTileAt : Int -> Int -> Int -> Int -> Tile -> List(Tile)
-isTileAt x1 y1 x2 y2 tile =
-  if x1 == x2 && y1 == y2 then
-    [tile]
-  else
-    []
-
-showMovements : Int -> Int -> List(List(Tile)) -> List(List(Tile))
-showMovements x y board =
-    List.map (\ tiles ->
-        List.map (\ tile ->
-          let distance = calculateDistance (Tuple.first (getTilePosition tile)) (Tuple.second (getTilePosition tile)) x y
-          in
-            if distance == 1 || distance == 2 then
-              PossibleMovement (x, y)
-            else if distance == 0 then
-              selectBlob tile
-            else
-              tile
-          ) tiles
-      ) board
-
-selectBlob : Tile -> Tile
-selectBlob tile =
-  case tile of
-    RedBlob (x, y) _ -> RedBlob (x, y) True
-    BlueBlob (x, y) _ -> BlueBlob (x, y) True
-    _ -> tile
-
-unselectBlob : Tile -> Tile
-unselectBlob tile =
-  case tile of
-    RedBlob (x, y) _ -> RedBlob (x, y) False
-    BlueBlob (x, y) _ -> BlueBlob (x, y) False
-    _ -> tile
-
-getTilePosition : Tile -> (Int, Int)
-getTilePosition tile =
-  case tile of
-    RedBlob (x, y) _-> (x, y)
-    BlueBlob (x, y) _ -> (x, y)
-    Empty (x, y) -> (x, y)
-    PossibleMovement (x, y) -> (x, y)
-
-validSelection : Int -> Int -> Turn -> List(List(Tile)) -> Bool
-validSelection x y turn board =
-    case turn of
-      BlueTurn ->
-        case getTile x y board of
-          Just tile ->
-            case tile of
-              BlueBlob _ _ -> True
-              _ -> False
-          _ -> False
-      RedTurn ->
-        case getTile x y board of
-          Just tile ->
-            case tile of
-              RedBlob _ _ -> True
-              _ -> False
-          _ -> False
-
-clearPossibleMovements : List(List(Tile)) -> List(List(Tile))
-clearPossibleMovements board =
-  List.map (\ tiles -> List.map (\ tile ->
-    case tile of
-      PossibleMovement (x, y) -> Empty (x, y)
-      _ -> unselectBlob tile
-    ) tiles) board
+calculateDistance : Point -> Point -> Distance
+calculateDistance p1 p2 =
+  max (abs (Tuple.first p1 - Tuple.first p2)) (abs (Tuple.second p1 - Tuple.second p2))
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  case model.winner of
-    BlueWin ->
+ case model.match of
+   BlueWin ->
       defaultViewWithContent [h2 [] [text "Blue wins!"]]
-    RedWin ->
+   RedWin ->
       defaultViewWithContent [h2 [] [text "Red wins!"]]
-    MatchInProgress ->
+   Tie ->
+      defaultViewWithContent [h2 [] [text "Tie!"]]
+   InProgress ->
       defaultViewWithContent [turnToHtml model.turn, table [] (boardToHtml model.board)]
 
 defaultViewWithContent : List(Html Msg) -> Html Msg
 defaultViewWithContent content =
-  div [] ([ h1 [] [ text "Blob Wars!" ]] ++ content)
+   div [] ([ h1 [] [ text "Blob Wars!" ]] ++ content)
 
 turnToHtml : Turn -> Html Msg
 turnToHtml turn =
-    case turn of
+   case turn of
       RedTurn -> h3 [] [text "Red turn..."]
       BlueTurn -> h3 [] [text "Blue turn..."]
 
 boardToHtml : List(List(Tile)) -> List(Html Msg)
 boardToHtml board =
-  List.map (\tiles ->
-        tr [] (List.map (\ elem ->
-            case elem of
-              Empty (x, y) -> td [onClick (Click (x, y))] [text "X"]
-              RedBlob (x, y) False -> td [onClick (Click (x, y))] [text "r"]
-              BlueBlob (x, y) False -> td [onClick (Click (x, y))] [text "b"]
-              RedBlob (x, y) True -> td [onClick (Click (x, y))] [text "R"]
-              BlueBlob (x, y) True -> td [onClick (Click (x, y))] [text "B"]
-              PossibleMovement (x, y) -> td [onClick (Click (x, y))] [text "o"]
-          ) tiles)) board
+   List.indexedMap (\x tiles ->
+      tr [] (List.indexedMap (\y tile ->
+               case tile of
+                  Empty            -> td [onClick (Click (x, y))] [text "X"]
+                  PossibleMovement -> td [onClick (Click (x, y))] [text "o"]
+                  Unselected blob ->
+                     case blob of
+                        Red        -> td [onClick (Click (x, y))] [text "r"]
+                        Blue       -> td [onClick (Click (x, y))] [text "b"]
+                  Selected blob ->
+                     case blob of
+                        Red        -> td [onClick (Click (x, y))] [text "R"]
+                        Blue       -> td [onClick (Click (x, y))] [text "B"]
+            ) tiles)
+   ) board
